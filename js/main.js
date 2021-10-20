de1eec58bfbff440233742c02cc015a19ae7fed7
Marcos Eduardo <markuzemb18@gmail.com>
2021-10-20 18:05:46 -0500

    Cambios nuevos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+window.addEventListener('load',function(){
+
+    
+
+    let formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit',()=>{
+        alert('Se ha registrado correctamente');
+    });
+    
+
+});
