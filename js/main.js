431805451409194ed53d69f67a0ab4d43401c823
Marcos Eduardo <markuzemb18@gmail.com>
2021-10-20 20:15:49 -0500

    Se agrego la opcion de genero

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 
 window.addEventListener('load',function(){
 
-    
-
     let formulario = document.getElementById('formulario');
     formulario.addEventListener('submit',()=>{
         alert('Se ha registrado correctamente');
